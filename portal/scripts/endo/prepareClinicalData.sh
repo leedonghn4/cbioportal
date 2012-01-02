a37b4209cd9a3a787acce0f6bc7656732c39f6ab
Ethan Cerami <cerami@gmail.com>
2012-01-02 16:42:39 -0500

    Changed final argument to directory location

--- portal/scripts/endo/prepareClinicalData.sh
+++ portal/scripts/endo/prepareClinicalData.sh
@@ -1,1 +1,1 @@
-./prepareClinicalData.pl ~/SugarSync/endo/data/ClinicalTable_12_30_2011.txt ~/SugarSync/endo/data/MSI_12_30_2011.txt ~/SugarSync/endo/data/UCEC.maf.annotated ~/SugarSync/endo/data/ClinicalData_Unified_12_30_2011.txt
+./prepareClinicalData.pl ~/SugarSync/endo/data/ClinicalTable_12_30_2011.txt ~/SugarSync/endo/data/MSI_12_30_2011.txt ~/SugarSync/endo/data/UCEC.maf.annotated ~/SugarSync/endo/data/out
