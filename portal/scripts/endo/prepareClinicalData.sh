7c9c46a99e2f9f4117647456787e6af9b611a78a
Ethan Cerami <cerami@gmail.com>
2012-01-03 14:35:58 -0500

    Added support for hyper mutated list

--- portal/scripts/endo/prepareClinicalData.sh
+++ portal/scripts/endo/prepareClinicalData.sh
@@ -1,1 +1,1 @@
-./prepareClinicalData.pl ~/SugarSync/endo/data/ClinicalTable_12_30_2011.txt ~/SugarSync/endo/data/MSI_12_30_2011.txt ~/SugarSync/endo/data/UCEC.maf.annotated ~/SugarSync/endo/data/out
+./prepareClinicalData.pl ~/SugarSync/endo/data/ClinicalTable_12_30_2011.txt ~/SugarSync/endo/data/MSI_12_30_2011.txt ~/SugarSync/endo/data/UCEC.maf.annotated ~/SugarSync/endo/data/hypermutated_cases.txt ~/SugarSync/endo/data/out
