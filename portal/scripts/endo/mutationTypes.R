29364cbbb3b9b010f762fa9534ec51ab1161f3ce
Ethan Cerami <cerami@gmail.com>
2012-01-13 13:38:55 -0500

    Adjusted some plots

--- portal/scripts/endo/mutationTypes.R
+++ portal/scripts/endo/mutationTypes.R
@@ -23,14 +23,14 @@ df = transform(df, CA_PERCENT = CA_COUNT/TOTAL_SNV_COUNT)
 df = transform(df, TOTAL_PERCENT=TG_PERCENT + TC_PERCENT + TA_PERCENT + CT_PERCENT + CG_PERCENT + CA_PERCENT)
 
 # Sort by TOTAL_SNV_COUNT
-sub_df = df[order (df$TOTAL_SNV_COUNT, decreasing=T),]
+sub_df = df[order (df$TOTAL_SNV_COUNT, decreasing=F),]
 
-p1 = qplot(1:nrow(sub_df), TG_PERCENT, data=sub_df, geom="bar", colour=MUTATION_RATE_CATEGORY, stat="identity") + opts(legend.position = "none") + xlab("All Cases")
-p2 = qplot(1:nrow(sub_df), TC_PERCENT, data=sub_df, geom="bar", colour=MUTATION_RATE_CATEGORY, stat="identity") + opts(legend.position = "none") + xlab("All Cases")
-p3 = qplot(1:nrow(sub_df), TA_PERCENT, data=sub_df, geom="bar", colour=MUTATION_RATE_CATEGORY, stat="identity") + opts(legend.position = "none") + xlab("All Cases")
-p4 = qplot(1:nrow(sub_df), CT_PERCENT, data=sub_df, geom="bar", colour=MUTATION_RATE_CATEGORY, stat="identity") + opts(legend.position = "none") + xlab("All Cases")
-p5 = qplot(1:nrow(sub_df), CG_PERCENT, data=sub_df, geom="bar", colour=MUTATION_RATE_CATEGORY, stat="identity") + opts(legend.position = "none") + xlab("All Cases")
-p6 = qplot(1:nrow(sub_df), CA_PERCENT, data=sub_df, geom="bar", colour=MUTATION_RATE_CATEGORY, stat="identity") + opts(legend.position = "none") + xlab("All Cases")
+p1 = qplot(1:nrow(sub_df), TG_PERCENT, data=sub_df, geom="bar", colour=MUTATION_RATE_CATEGORY, stat="identity") + opts(legend.position = "none") + xlab("All Cases") + ylim(0,1)
+p2 = qplot(1:nrow(sub_df), TC_PERCENT, data=sub_df, geom="bar", colour=MUTATION_RATE_CATEGORY, stat="identity") + opts(legend.position = "none") + xlab("All Cases") + ylim(0,1)
+p3 = qplot(1:nrow(sub_df), TA_PERCENT, data=sub_df, geom="bar", colour=MUTATION_RATE_CATEGORY, stat="identity") + opts(legend.position = "none") + xlab("All Cases") + ylim(0,1)
+p4 = qplot(1:nrow(sub_df), CT_PERCENT, data=sub_df, geom="bar", colour=MUTATION_RATE_CATEGORY, stat="identity") + opts(legend.position = "none") + xlab("All Cases") + ylim(0,1)
+p5 = qplot(1:nrow(sub_df), CG_PERCENT, data=sub_df, geom="bar", colour=MUTATION_RATE_CATEGORY, stat="identity") + opts(legend.position = "none") + xlab("All Cases") + ylim(0,1)
+p6 = qplot(1:nrow(sub_df), CA_PERCENT, data=sub_df, geom="bar", colour=MUTATION_RATE_CATEGORY, stat="identity") + opts(legend.position = "none") + xlab("All Cases") + ylim(0,1)
 
 grid.arrange(p1, p2, p3, p4, p5, p6, nrow=2)
 
@@ -43,7 +43,6 @@ p1 = ggplot(melted_df_1, aes(x=x1, y=value, fill=metric)) + geom_bar()
 p1 = p1 + opts(title="Highest Mutation Group")
 p1 = p1 + opts(axis.text.x = theme_blank())
 p1 = p1 + xlab("All Cases")
-p1 = p1 + opts(legend.position = "none") 
 p1 = p1 + ylab("Proportion of all SNVs")
 
 local_df_2 = subset(sub_df, MUTATION_RATE_CATEGORY=="2_HIGH", select=c(TOTAL_SNV_COUNT, CASE_ID, TG_PERCENT, TC_PERCENT, TA_PERCENT, CT_PERCENT, CG_PERCENT, CA_PERCENT))
@@ -52,11 +51,34 @@ x2 = reorder(melted_df_2$CASE_ID, melted_df_2$TOTAL_SNV_COUNT)
 #p2 = ggplot(melted_df_2, aes(x=x2, y=value, group=metric, fill=metric)) + geom_area()
 p2 = ggplot(melted_df_2, aes(x=x2, y=value, fill=metric)) + geom_bar() 
 p2 = p2 + opts(title="High Mutation Group")
-p2 = p2 + opts(axis.text.x = theme_blank())
 p2 = p2 + xlab("All Cases")
 p2 = p2 + ylab("Proportion of all SNVs")
+p2 = p2 + opts(axis.text.x = theme_blank())
+p2 = p2 + opts(legend.position = "none") 
 
-grid.arrange(p1, p2, nrow=1)
+grid.arrange(p2, p1, nrow=1)
+
+# Compare HIGHEST v. HIGH Directly
+local_df_1 = subset(sub_df, MUTATION_RATE_CATEGORY=="1_HIGHEST", select=c(TOTAL_SNV_COUNT, CASE_ID, TG_PERCENT, TC_PERCENT, TA_PERCENT, CT_PERCENT, CG_PERCENT, CA_PERCENT))
+melted_df_1 = melt(local_df_1, id=c("CASE_ID", "TOTAL_SNV_COUNT"), variable_name="metric")
+x1 = reorder(melted_df_1$CASE_ID, melted_df_1$TOTAL_SNV_COUNT)
+p1 = ggplot(melted_df_1, aes(x=x1, y=value, group=metric, fill=metric)) + geom_area() 
+p1 = p1 + opts(title="Highest Mutation Group")
+p1 = p1 + opts(axis.text.x = theme_blank())
+p1 = p1 + xlab("All Cases")
+p1 = p1 + ylab("Proportion of all SNVs")
+
+local_df_2 = subset(sub_df, MUTATION_RATE_CATEGORY=="2_HIGH", select=c(TOTAL_SNV_COUNT, CASE_ID, TG_PERCENT, TC_PERCENT, TA_PERCENT, CT_PERCENT, CG_PERCENT, CA_PERCENT))
+melted_df_2 = melt(local_df_2, id=c("CASE_ID", "TOTAL_SNV_COUNT"), variable_name="metric")
+x2 = reorder(melted_df_2$CASE_ID, melted_df_2$TOTAL_SNV_COUNT)
+p2 = ggplot(melted_df_2, aes(x=x2, y=value, group=metric, fill=metric)) + geom_area()
+p2 = p2 + opts(title="High Mutation Group")
+p2 = p2 + xlab("All Cases")
+p2 = p2 + ylab("Proportion of all SNVs")
+p2 = p2 + opts(axis.text.x = theme_blank())
+p2 = p2 + opts(legend.position = "none") 
+
+grid.arrange(p2, p1, nrow=1)
 
 
 # Box Plots
