afff83c936b64f72bc3ea25f4e6a5f864e198429
Ethan Cerami <cerami@gmail.com>
2012-01-30 13:52:14 -0500

    Continued MSI research

--- portal/scripts/endo/msi.R
+++ portal/scripts/endo/msi.R
@@ -1,15 +1,15 @@
 # Read in Unified Clinical File
-df = read.delim("~/SugarSync/endo/data/out/ucec_clinical_unified.txt")
+df = read.delim("~/SugarSync/endo/data/out/ucec_clinical_with_clusters_unified.txt")
 
-# First, only get cases for which we have definitive MSI results
-df_sub <- subset(df, MSI_STATUS=="MSI-H" | MSI_STATUS=="MSI-L" | MSI_STATUS=="MSS")
+# Filter:
+# 1. only sequenced cases
+# 2. only cases that have a MUTATION_RATE_CLUSTER
+# 2. only get cases for which we have definitive MSI results
+df_sub = subset(df, SEQUENCED=="Y")
+df_sub = subset (df_sub, MUTATION_RATE_CLUSTER != "NA")
+df_sub <- subset(df_sub, MSI_STATUS=="MSI-H" | MSI_STATUS=="MSI-L" | MSI_STATUS=="MSS")
 
-# Create new MSI_HIGH Column
-df_sub = transform(df_sub, MSI_HIGH=0)
-df_sub[df_sub$MSI_STATUS=="MSI-H",]$MSI_HIGH=1
-
-# Focus on MUTATION_RATE_CATEGORY:  HIGHEST v. HIGH
-t = table(df_sub$MSI_HIGH, df_sub$MUTATION_RATE_CATEGORY, exclude="3_LOW")
+t = table(df_sub$MSI_STATUS, df_sub$MUTATION_RATE_CLUSTER, exclude=c("Indeterminant", "Not Done"))
 prop.table(t, 2)
 
 fisher.test(t)
