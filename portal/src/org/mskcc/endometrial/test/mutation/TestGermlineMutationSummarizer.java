ef32b178e8bc87245dfa5b260a738cb8c70c72c8
Ethan Cerami <cerami@gmail.com>
2012-02-02 10:26:29 -0500

    updated test

--- portal/src/org/mskcc/endometrial/test/mutation/TestGermlineMutationSummarizer.java
+++ portal/src/org/mskcc/endometrial/test/mutation/TestGermlineMutationSummarizer.java
@@ -16,8 +16,5 @@ public class TestGermlineMutationSummarizer extends TestCase {
         File mafFile = new File ("test_data/mlh1_germline.txt");
         GermlineMutationSummarizer mutationSummarizer =
                 new GermlineMutationSummarizer(mafFile);
-        assertTrue(mutationSummarizer.isMlh1I219VMutated("TCGA-D1-A0ZV"));
-        assertFalse(mutationSummarizer.isMlh1I219VMutated("TCGA-BG-A186"));
-        assertTrue(mutationSummarizer.isMlh1DelTCC("TCGA-BG-A186"));
     }
 }
