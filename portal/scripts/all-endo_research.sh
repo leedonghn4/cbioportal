67e17c54c33221ab4745970d2750b2d1c5319078
Ethan Cerami <cerami@gmail.com>
2012-03-03 15:16:22 -0500

    Modified script to only required data checked into google code

--- portal/scripts/all-endo_research.sh
+++ portal/scripts/all-endo_research.sh
@@ -1,14 +1,11 @@
-# First, verify if all symbols in the sample genesets are latest
-./verifyGeneSets.pl $CGDS_DATA_HOME/human_genes.txt
-
 # Clear the Database
 ./resetDb.pl
 
 # Load up Entrez Genes
-./importGenes.pl $CGDS_DATA_HOME/human_genes.txt
+./importGenes.pl ../sample_data/genes/human_genes.txt
 
-# Load up Cancer Types
-./importTypesOfCancer.pl $CGDS_DATA_HOME/cancers.txt
+# Load up All Cancer Types
+./importTypesOfCancer.pl ../sample_data/cancers.txt
 
 # Load up Sanger Cancer Gene Census
 ./importSangerCensus.pl ../sample_data/genes/sanger_gene_census.txt
